5a9b002b1c36f6ec8144afc1f72921e5cdddc2e6
Vasiliy <yaronskii@phystech.edu>
2016-06-15 01:52:55 +0300

    check php forms

---
+++ action.php
@@ -0,0 +1,13 @@
+<html>
+ <head>
+  <title>Тестируем PHP</title>
+ </head>
+ <body>
+ <?php echo '<p>Привет, мир!</p>'; ?>
+ <?php 
+   $text =  ($_POST['mytext']; 
+   $msg =  ($_POST['mytext']; 
+   echo $text; // Выводим содержимое текстового поля 
+ ?>
+ </body>
+</html>
